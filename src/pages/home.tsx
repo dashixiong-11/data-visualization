f868bd52297133affac56c0772dc65d65bf2b713
dashixiong <2788347834@qq.com>
2021-12-10 21:09:04 +0800

    更换背景， header 图片

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.scss';
-import headerBg from '../images/header.png';
+import headerBg from '../images/head_bg.png';
+import homeBg from '../images/bj.png';
 import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
 import {Chart3} from '../components/chart-3';
@@ -17,57 +18,72 @@ import {Chart13} from '../components/chart-13';
 import {Chart14} from '../components/chart-14';
 
 export const Home = () => {
-  const year = new Date().getFullYear();
-  return (
-    <div className="home">
-      <header style={{backgroundImage: `url(${headerBg})`}}/>
-      <main>
-        <section className="section1">
-          <Chart1/>
-          <Chart2/>
-        </section>
-        <section className="section2">
-          <Chart3/>
-          <Chart4/>
-        </section>
-        <section className="bordered section3">
-          <Chart5/>
-        </section>
-        <section className="section4">
-          <Chart6/>
-          <div className="bordered 年龄段">
-            <h2>犯罪人员年龄段分布</h2>
-            <div className="charts">
-              <Chart7/>
-              <Chart8/>
-              <Chart9/>
-            </div>
-          </div>
-        </section>
-        <section className="section5">
-          <div className="bordered row1 案发类型">
-            <h2>案发类型统计</h2>
-            <div className="charts">
-              <Chart10/>
-              <Chart11/>
-            </div>
-          </div>
-          <div className="bordered row2 案发街道">
-            <h2>案发街道统计</h2>
-            <div className="charts">
-              <Chart12/>
-              <Chart13/>
-            </div>
-          </div>
-          <div className="bordered row3 作案手段">
-            <h2>作案手段分析</h2>
-            <Chart14/>
-          </div>
-        </section>
-      </main>
-      <footer>
-        &copy; 饥人谷 2020-{year}
-      </footer>
-    </div>
-  );
+    const year = new Date().getFullYear();
+
+    function GetDateDiff(startDate) {
+        let startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
+        let endTime = Date.now()
+        let res = parseInt(Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24) + '')
+        return res;
+    }
+
+    const time = GetDateDiff('2019-01-01')
+    return (
+        <div className="home" style={{backgroundImage: `url(${homeBg})`}}>
+            <header style={{backgroundImage: `url(${headerBg})`}}>
+                <span className='header-time'>已安全运行 {time} 天 </span>
+                <span className='header-title'>
+                    xxx系统
+                </span>
+                <div className='header-logout'>退出登录</div>
+            </header>
+            <main>
+                <section className="section1">
+                    <Chart1/>
+                    <Chart2/>
+                </section>
+                <section className="section2">
+                    <Chart3/>
+                    <Chart4/>
+                </section>
+                <section className="bordered section3">
+                    <Chart5/>
+                </section>
+                <section className="section4">
+                    <Chart6/>
+                    <div className="bordered 年龄段">
+                        <h2>犯罪人员年龄段分布</h2>
+                        <div className="charts">
+                            <Chart7/>
+                            <Chart8/>
+                            <Chart9/>
+                        </div>
+                    </div>
+                </section>
+                <section className="section5">
+                    <div className="bordered row1 案发类型">
+                        <h2>案发类型统计</h2>
+                        <div className="charts">
+                            <Chart10/>
+                            <Chart11/>
+                        </div>
+                    </div>
+                    <div className="bordered row2 案发街道">
+                        <h2>案发街道统计</h2>
+                        <div className="charts">
+                            <Chart12/>
+                            <Chart13/>
+                        </div>
+                    </div>
+                    <div className="bordered row3 作案手段">
+                        <h2>作案手段分析</h2>
+                        <Chart14/>
+                    </div>
+                </section>
+            </main>
+            <footer>
+                &copy; xxx公司 2021-{year}
+            </footer>
+        </div>
+    );
 };
